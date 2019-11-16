8ed6be568ca770a2c53ddd46923755fa34ba299a
tsuen4 <ht17a045@oecu.jp>
2019-11-17 02:52:59 +0900

    favicon を変える時の土台を作成 (Vue のアイコンがデフォルトでついてくるので)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,9 @@
 module.exports = {
   base: '/flow/',
   title: 'OECU-Git-Learning: GitHub Flow',
+  head: [
+    // ['link', { rel: 'icon', href: '' }]
+  ],
   markdown: {
     config: (md) => {
       md.options.linkify = true
